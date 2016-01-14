8c7ca0b2a97cea47cb588c95769f44e3e083b56a
kgadams93 <kgadams93@gmail.com>
2016-01-14 16:22:23 +0000

    Create new template, action, and routes for articles

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,19 @@
+class ArticlesController < ApplicationController
+  
+  def new
+    @article = Article.new
+  end
+  
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to articles_show(@article)
+  end
+  
+  private
+  
+    def article_params
+      params.require(:article).permit(:title, :country, :divesite, :description)
+    end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   
   get '/about', to: 'pages#about'
   
+  resources :articles
+  
 end
