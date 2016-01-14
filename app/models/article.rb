514548516343a2a2336f98a1fc96fe3614deb2cc
kgadams93 <kgadams93@gmail.com>
2016-01-14 15:31:52 +0000

    Add article validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,8 @@
 class Article < ActiveRecord::Base
   
-  
+  validates :title, presence: true, length: { minimum: 2, maximum: 50 }
+  validates :country, presence: true, length: { minimum: 2, maximum: 100 }
+  validates :divesite, length: { minimum: 2, maximum: 50 }
+  validates :description, presence: true, length: { minimum: 10 }
   
 end
