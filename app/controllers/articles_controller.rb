02bfcca387c084b57e6762b41fb3cdf3a168425a
kgadams93 <kgadams93@gmail.com>
2016-01-14 16:47:23 +0000

    Add create and show actions for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,8 +6,16 @@ class ArticlesController < ApplicationController
   
   def create
     @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
+    if @article.save
+      flash[:success] = "Article was created successfully."
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
+  
+  def show
+    @article = Article.find(params[:id])
   end
   
   private
