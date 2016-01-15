6e5b767a2da40b34312825bfc9d85d562893528c
kgadams93 <kgadams93@gmail.com>
2016-01-15 09:54:29 +0000

    Add update action to articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,6 +18,16 @@ class ArticlesController < ApplicationController
     end
   end
   
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:success] = "Article was updated successfully."
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+  
   def show
     @article = Article.find(params[:id])
   end
