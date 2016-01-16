b5e2f21754773dcb53bcc450459aca68217515e1
kgadams93 <kgadams93@gmail.com>
2016-01-16 21:25:40 +0000

    Add user sessions

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   get '/about', to: 'pages#about'
   get '/signup', to: 'users#new'
   
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
   resources :articles
   resources :users, except: [:new]
   
