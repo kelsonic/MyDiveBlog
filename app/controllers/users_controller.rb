6a112d5cde15c86f9978ca13f86255d5a87746f1
kgadams93 <kgadams93@gmail.com>
2016-01-16 14:48:32 +0000

    Create show action and add gravatar image

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   
+  before_action :set_user, only: [:edit, :update, :show, :destroy]
+  
   def new
     @user = User.new
   end
@@ -15,11 +17,9 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id])
   end
   
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = "Your account was updated successfully."
       redirect_to articles_path
@@ -28,10 +28,17 @@ class UsersController < ApplicationController
     end
   end
   
+  def show
+  end
+  
   private
   
     def user_params
       params.require(:user).permit(:username, :email, :password)
+    end
+    
+    def set_user
+      @user = User.find(params[:id])
     end
   
 end
