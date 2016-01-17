772fb0cd9a66a1a3e09d1819108024613753b574
kgadams93 <kgadams93@gmail.com>
2016-01-17 18:20:35 +0000

    Edit invalid category submission test

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -12,6 +12,16 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
     assert_match 'Altitude Diving', response.body
   end
   
+  test "invalid category submission results in failure" do
+    get new_category_path
+    assert_template 'categories/new'
+    assert_no_difference 'Category.count' do
+      post categories_path, category: {name: ""}
+    end
+    assert_template 'categories/new'
+    assert_select 'h2.panel-title'
+    assert_select 'div.panel-body'
+  end
   
   
 end
