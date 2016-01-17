9e1ca1eccb1ae7b79a08fb708a71cea2576c4ac1
kgadams93 <kgadams93@gmail.com>
2016-01-17 13:22:25 +0000

    Add sign in directly after sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,8 +15,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to My Dive Blog #{@user.username}"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render 'new'
     end
