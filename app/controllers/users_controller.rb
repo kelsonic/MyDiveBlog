42678b9e21ebd05a334acba21bdcb208d36eadfd
kgadams93 <kgadams93@gmail.com>
2016-01-17 12:04:59 +0000

    Add user dropdown menu in navbar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   
-  before_action :set_user, only: [:edit, :update, :show, :destroy]
+  before_action :set_user, only: [:edit, :update, :show]
   
   def index
     @users = User.paginate(:page => params[:page], :per_page => 5)
