06db43cb135e61f3d056cfac736949d15a0837af
kgadams93 <kgadams93@gmail.com>
2016-01-18 11:11:37 +0000

    Add article and category association

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,8 @@
 class Category < ActiveRecord::Base
   
+  has_many :article_categories
+  has_many :articles, through: :article_categories
+  
   validates :name, presence: true, length: { minimum: 3, maximum: 30 }
   validates_uniqueness_of :name
   
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160117170827) do
+ActiveRecord::Schema.define(version: 20160118105951) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer  "article_id"
+    t.integer  "category_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
