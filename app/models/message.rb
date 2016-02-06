1e502ead778793a372e906365f4c97cb7da59980
kgadams93 <kgadams93@gmail.com>
2016-02-06 10:57:57 +0000

    Create messages model

---
+++ app/models/message.rb
@@ -0,0 +1,8 @@
+class Message < ActiveRecord::Base
+  
+  belongs_to :conversation
+  belongs_to :user
+  
+  validates_presence_of :body, :conversation_id, :user_id
+  
+end
---
+++ db/migrate/20160206104900_create_messages.rb
@@ -0,0 +1,9 @@
+class CreateMessages < ActiveRecord::Migration
+  def change
+    create_table :messages do |t|
+      t.text :body
+      t.references :conversation, index: true, foreign_key: true
+      t.references :user, index: true, foreign_key: true
+    end
+  end
+end
