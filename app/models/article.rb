793ac56e49ce8776d4ce77e1cd73af1774bfbc98
kgadams93 <kgadams93@gmail.com>
2016-05-15 15:46:08 +0000

    Update gemfile

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,7 +5,7 @@ class Article < ActiveRecord::Base
   has_many :categories, through: :article_categories
   
   validates :title, presence: true, length: { minimum: 2, maximum: 50 }
-  validates :country, presence: true, length: { minimum: 2, maximum: 100 }
+  validates :country, presence: true, length: { minimum: 2, maximum: 10 }
   validates :divesite, length: { minimum: 2, maximum: 50 }
   validates :description, presence: true, length: { minimum: 10 }
   validates :user_id, presence: true
